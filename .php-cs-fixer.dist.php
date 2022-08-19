cad98a795b47c3a11d4034c082d167114912fb6e
yannickfrd <farade.y@hotmail.fr>
2022-08-19 18:47:19 +0200

    feat: install and config phpcs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->exclude('config')
+    ->exclude('node_modules')
+    ->exclude('public')
+    ->exclude('vendor')
+    ->notPath('bin/console')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PhpCsFixer' => true,
+        '@PhpCsFixer:risky' => true,
+        '@PSR12' => true,
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+    ])
+//    ->setLineEnding(PHP_EOL)
+    ->setFinder($finder)
+    ->setCacheFile(__DIR__.'/var/.php_cs.cache')
+    ;
